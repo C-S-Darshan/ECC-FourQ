054527d9f1146f543629a3d949e7d9cd88897dcd
C-S-Darshan <darshan.cs1113@gmail.com>
2024-01-22 19:21:06 +0530

    test commit

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"io"
-  "encoding/hex"
+
 	"github.com/cloudflare/circl/ecc/fourq"
 )
 
 // 32 byte keys used
-const Size = 32 // 
+const Size = 32 //
 type Key [Size]byte
 
 // From secret s, calculate public key (public=aG)
 func KeyGen(public, s *Key) {
-  var P fourq.Point
-  P.ScalarBaseMult((*[32]byte)(s))
-  P.Marshal((*[Size]byte)(public))
+	var P fourq.Point
+	P.ScalarBaseMult((*[32]byte)(s))
+	P.Marshal((*[Size]byte)(public))
 
 }
 
@@ -36,26 +37,28 @@ func main() {
 		AlicePublic, BobPublic,
 		AliceShared, BobShared Key
 
-  // Generate Alice's private key and public key
+	// Generate Alice's private key and public key
 	_, _ = io.ReadFull(rand.Reader, AliceSecret[:32])
 	KeyGen(&AlicePublic, &AliceSecret)
 
-  // Generate Bob's private key and public key
+	// Generate Bob's private key and public key
 	_, _ = io.ReadFull(rand.Reader, BobSecret[:])
 	KeyGen(&BobPublic, &BobSecret)
 
-  fmt.Println("Fourq key sharing")
-  fmt.Println("Alice Secret: ", hex.EncodeToString(AliceSecret[:32]))
-	fmt.Println("Alice Public: ",hex.EncodeToString(AlicePublic[:32]))
-  fmt.Println("\n\nBob Secret: ", hex.EncodeToString(BobSecret[:32]))
-	fmt.Println("Bob Public: ",hex.EncodeToString(BobPublic[:32]))
+	fmt.Println("Fourq key sharing")
+	fmt.Println("Alice Secret: ", hex.EncodeToString(AliceSecret[:32]))
+	fmt.Println("Alice Public: ", hex.EncodeToString(AlicePublic[:32]))
+	fmt.Println("\n\nBob Secret: ", hex.EncodeToString(BobSecret[:32]))
+	fmt.Println("Bob Public: ", hex.EncodeToString(BobPublic[:32]))
 
 	// Determine shared keys
 	Shared(&AliceShared, &AliceSecret, &BobPublic)
 	Shared(&BobShared, &BobSecret, &AlicePublic)
 
-	fmt.Println("\n\nBob Shared:\t", hex.EncodeToString( BobShared[:32] ))
-  fmt.Println("Alice Shared:\t", hex.EncodeToString( AliceShared[:32] ))
-
+	fmt.Println("\n\nBob Shared:\t", hex.EncodeToString(BobShared[:32]))
+	fmt.Println("Alice Shared:\t", hex.EncodeToString(AliceShared[:32]))
+	//checking if if this works
+	var name string = "Darshan"
+	fmt.Println(name)
 
 }
